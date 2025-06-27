c93defe85a3f72a739ac1493e1cf10e1d835b8ed
Shahriar Hossain Rafi <106436124+Srafi3107@users.noreply.github.com>
2025-06-27 20:15:02 +0600

    Update Lowest Common Ancestor of a Binary Tree.cpp

--- Leet Code 150/Lowest Common Ancestor of a Binary Tree.cpp
+++ Leet Code 150/Lowest Common Ancestor of a Binary Tree.cpp
@@ -6,14 +6,11 @@ public:
         if (root == nullptr || root == p || root == q)
             return root;
 
-       
         TreeNode* left = lowestCommonAncestor(root->left, p, q);
         TreeNode* right = lowestCommonAncestor(root->right, p, q);
-
         
         if (left != nullptr && right != nullptr)
             return root;
-
       
         return (left != nullptr) ? left : right;
     }
