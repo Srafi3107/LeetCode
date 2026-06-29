ea225720a81b959e3007c78e43551b72c3c5d43e
Shahriar Hossain Rafi <106436124+Srafi3107@users.noreply.github.com>
2026-06-29 18:20:07 +0600

    Update Add Binary.cpp

--- Add Binary.cpp
+++ Add Binary.cpp
@@ -1,36 +1,25 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-using namespace std;
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result = "";
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        int carry = 0;
 
-string addBinary(string a, string b) {
-    string result = "";
-    int i = a.length() - 1;
-    int j = b.length() - 1;
-    int carry = 0;
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
 
-    while (i >= 0 || j >= 0 || carry) {
-        int sum = carry;
+            if (i >= 0)
+                sum += a[i--] - '0';
 
-        if (i >= 0)
-            sum += a[i--] - '0';
+            if (j >= 0)
+                sum += b[j--] - '0';
 
-        if (j >= 0)
-            sum += b[j--] - '0';
+            result += (sum % 2) + '0';
+            carry = sum / 2;
+        }
 
-        result += (sum % 2) + '0';
-        carry = sum / 2;
+        reverse(result.begin(), result.end());
+        return result;
     }
-
-    reverse(result.begin(), result.end());
-    return result;
-}
-
-int main() {
-    string a = "1010";
-    string b = "1011";
-
-    cout << addBinary(a, b) << endl;
-
-    return 0;
-}
+};
