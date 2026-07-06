d41e501e82f4c6db91523875578fb33992c3accf
Shahriar Hossain Rafi <106436124+Srafi3107@users.noreply.github.com>
2026-07-06 20:32:15 +0600

    Update Search Insert Position.cpp

--- Search Insert Position.cpp
+++ Search Insert Position.cpp
@@ -1,15 +1,19 @@
-class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
-        left, right = 0, len(nums) - 1
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
 
-        while left <= right:
-            mid = (left + right) // 2
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid - 1
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
 
-        return left
+        return left;
+    }
+};
