ad9822411850c3c4bcd1baf2d9bcfd6d07c1e87c
Shahriar Hossain Rafi <106436124+Srafi3107@users.noreply.github.com>
2026-07-07 21:54:02 +0600

    Update Search Insert Position.cpp

--- Search Insert Position.cpp
+++ Search Insert Position.cpp
@@ -9,7 +9,7 @@ public:
             if (nums[mid] == target)
                 return mid;
             else if (nums[mid] < target)
-                left = mid + 2;
+                left = mid + 1;
             else
                 right = mid - 1;
         }
