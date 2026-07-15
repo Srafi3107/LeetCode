aa18a30683e45b33e9ee1ee80c464fbd8d415e0e
Shahriar Hossain Rafi <106436124+Srafi3107@users.noreply.github.com>
2026-07-15 21:03:14 +0600

    Update Next Permutation.cpp

--- Next Permutation.cpp
+++ Next Permutation.cpp
@@ -1,15 +1,24 @@
-#include <iostream>
-#include <algorithm>
-using namespace std;
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
 
-int main() {
-    int arr[] = {1, 2, 3};
+        // Step 1: Find pivot
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
 
-    do {
-        for (int x : arr)
-            cout << x << " ";
-        cout << endl;
-    } while (next_permutation(arr, arr + 3));
+        // Step 2: Find next greater element and swap
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
 
-    return 0;
-}
+        // Step 3: Reverse suffix
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
