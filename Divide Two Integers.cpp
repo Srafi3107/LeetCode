284b0ba1946c810c08cacb202956b4294235dd94
Shahriar Hossain Rafi <106436124+Srafi3107@users.noreply.github.com>
2026-07-21 10:02:32 +0600

    Update Divide Two Integers.cpp

--- Divide Two Integers.cpp
+++ Divide Two Integers.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         
-        // Special overflow case
         if (dividend == INT_MIN && divisor == -1)
             return INT_MAX;
 
-        // Determine sign
         bool negative = (dividend < 0) ^ (divisor < 0);
 
-        // Convert to long long before taking absolute value
         long long a = abs((long long)dividend);
         long long b = abs((long long)divisor);
 
@@ -19,7 +16,6 @@ public:
             long long temp = b;
             long long multiple = 1;
 
-            // Double divisor while possible
             while ((temp << 1) <= a) {
                 temp <<= 1;
                 multiple <<= 1;
