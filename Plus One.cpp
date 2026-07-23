6aeb438e63782b475fa5519fa5a573f3ef92f24c
Shahriar Hossain Rafi <106436124+Srafi3107@users.noreply.github.com>
2026-07-23 22:17:03 +0600

    Create Plus One.cpp

---
+++ Plus One.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        
+        // Start from the last digit
+        for (int i = digits.size() - 1; i >= 0; i--) {
+            
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            
+            // 9 + 1 = 10
+            digits[i] = 0;
+        }
+
+        // If all digits were 9
+        digits.insert(digits.begin(), 1);
+
+        return digits;
+    }
+};
