3bc0b0e656d35f11b6d1ef3cc2307b7bca43919f
Shahriar Hossain Rafi <106436124+Srafi3107@users.noreply.github.com>
2026-07-23 22:16:23 +0600

    Update Valid Number.cpp

--- Valid Number.cpp
+++ Valid Number.cpp
@@ -24,7 +24,7 @@ public:
                     return false;
                 }
 
-                dotSeen = ;
+                dotSeen = true ;
             }
 
             else if (c == 'e' || c == 'E') {
