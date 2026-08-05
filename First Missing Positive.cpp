891d2d94d876c89f5eadf5dc462dbb5151256a09
Shahriar Hossain Rafi <106436124+Srafi3107@users.noreply.github.com>
2026-08-05 20:24:50 +0600

    Update First Missing Positive.cpp

--- First Missing Positive.cpp
+++ First Missing Positive.cpp
@@ -1,29 +1,25 @@
-#include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int firstMissingPositive(vector<int>& nums) {
-    int n = nums.size();
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        int n = nums.size();
 
-    // Place each number in its correct position
-    for (int i = 0; i < n; i++) {
-        while (nums[i] > 0 && nums[i] <= n &&
-               nums[nums[i] - 1] != nums[i]) {
-            swap(nums[i], nums[nums[i] - 1]);
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n &&
+                   nums[nums[i] - 1] != nums[i]) {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
         }
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
     }
-
-    // Find the first missing positive
-    for (int i = 0; i < n; i++) {
-        if (nums[i] != i + 1)
-            return i + 1;
-    }
-
-    return n + 1;
-}
-
-int main() {
-    vector<int> nums = {3, 4, -1, 1};
-    cout << firstMissingPositive(nums);
-    return 0;
-}
+};
