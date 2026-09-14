87843fb34168a091e285dffefde0401ef83f1c4e
Shahriar Hossain Rafi <106436124+Srafi3107@users.noreply.github.com>
2026-09-14 11:59:37 +0600

    Implement Edit Distance algorithm in C++

---
+++ Edit Distance.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+
+        // Convert empty word1 to word2
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+
+        // Convert word1 to empty word2
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                if (word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else {
+                    dp[i][j] = 1 + min({
+                        dp[i - 1][j],     // Delete
+                        dp[i][j - 1],     // Insert
+                        dp[i - 1][j - 1]  // Replace
+                    });
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+};
