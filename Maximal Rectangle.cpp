51a22343a0e752bf68fbf3390dffc52aced33c71
Shahriar Hossain Rafi <106436124+Srafi3107@users.noreply.github.com>
2026-09-17 18:18:33 +0600

    Create Maximal Rectangle.cpp

---
+++ Maximal Rectangle.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+
+    int largestRectangleArea(vector<int>& heights) {
+        stack<int> st;
+        int n = heights.size();
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; i++) {
+            int currentHeight = (i == n) ? 0 : heights[i];
+
+            while (!st.empty() && heights[st.top()] > currentHeight) {
+                int height = heights[st.top()];
+                st.pop();
+
+                int width;
+
+                if (st.empty())
+                    width = i;
+                else
+                    width = i - st.top() - 1;
+
+                maxArea = max(maxArea, height * width);
+            }
+
+            st.push(i);
+        }
+
+        return maxArea;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if (matrix.empty())
+            return 0;
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        vector<int> heights(cols, 0);
+        int answer = 0;
+
+        for (int i = 0; i < rows; i++) {
+
+            // Build histogram for this row
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1')
+                    heights[j]++;
+                else
+                    heights[j] = 0;
+            }
+
+            // Find largest rectangle in this histogram
+            answer = max(answer, largestRectangleArea(heights));
+        }
+
+        return answer;
+    }
+};
